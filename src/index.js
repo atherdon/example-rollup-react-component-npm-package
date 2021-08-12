62c6c12457125f33b137fa508b58c5009a74f060
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-12 12:18:42 +0000

    updating code for new components from misc folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@
 import AnExample from './AnExample/index.jsx';
 import AnotherExample from './AnotherExample/index.jsx';
 
+import Address from './address';
+import Copyright from './copyrights';
+import headStyles from './head-styles';
+import NewsletterSponsorshipLink from './newsletter-sponsorship-link';
+import Unsubscribe from './unsubscribe';
+
+
 export {
   AnExample,
   AnotherExample,
+
+  Address, Copyright, headStyles, NewsletterSponsorshipLink, Unsubscribe
 };
