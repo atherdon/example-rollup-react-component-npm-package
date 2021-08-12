18d2cf15104baee1f4d70e282d17af55f322ac2a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-08-12 12:33:34 +0000

    small upd

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 import AnExample from './AnExample/index.jsx';
 import AnotherExample from './AnotherExample/index.jsx';
 
-import Address from './address';
-import Copyright from './copyrights';
-import headStyles from './head-styles';
-import NewsletterSponsorshipLink from './newsletter-sponsorship-link';
-import Unsubscribe from './unsubscribe';
+import Address from './address/Address.jsx';
+import Copyright from './copyrights/Copyrights.jsx';
+import headStyles from './head-styles/index.js';
+import NewsletterSponsorshipLink from './newsletter-sponsorship-link/Newsletter.jsx';
+import Unsubscribe from './unsubscribe/Unsubscribe.jsx';
 
 
 export {
